3d38ca14aee9bdf803a409f52f71d2c0e58af24d
Justin Mancherje <mancherje.justin@gmail.com>
2016-01-06 18:53:15 -0800

    add templates and controllers for signup and login pages, add .bowerrc to move bower_components file

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,15 @@
-angular.module('app', ['app.login'])
+angular.module('app', ['app.login', 'app.signup','ngRoute'])
 
 // template for setting up routes
 .config(function ($routeProvider) {
   $routeProvider
-    .when('/', {
-
+    .when('/login', {
+      templateUrl: './js/templates/login.html',
+      controller: 'LoginController'
+    })
+    .when('/signup', {
+      templateUrl: './js/templates/signup.html',
+      controller: 'SignupController'
     })
     .otherwise('/');
 });
