cdabe56e6e7bbce8b229fdbc6b72852bdd13efa0
Liza Lopez <liza.b.lopez@gmail.com>
2016-01-06 18:54:14 -0800

    Set up route paths on router, directing to controller

--- server/server.js
+++ server/server.js
@@ -3,18 +3,55 @@ var path = require('path');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
-var router = express.Router();
-//var router = require(./routers/router.js);
+var router = require(./routers/router.js);
+// app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.json());
 
 var app = express();
 var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname+'/../public'));
-//app.use(express.static(path.join(__dirname, "../client")));
 
 
 app.listen(port);
 console.log('Listening on ' + port);
 
-// module.exports = app; 
+module.exports = app; 
 
+/*
+file system
+-Server 
+  -server.js - init server
+  -Users
+    -usersModel
+    -usersController
+  -Trips
+    -tripsModel
+    -tripsController
+  -ApiData
+    -ApiDModel
+    -ApliController
+-Config
+  -helper: error logger and handler?
+  -routes
+  -utils: get urlTitle, isValidUrl? 
+
+-Server 
+  -server.js - init server
+  -Controllers
+    -usersController
+    -tripsController
+    -ApiController
+  -Models
+    -usersModel
+    -tripsModel
+    -ApiDModel
+  -Routers
+    -router.js
+-Config
+  -helper: error logger and handler?
+  -routes
+  -utils: get urlTitle, isValidUrl? 
+
+
+*/
