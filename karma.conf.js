37f09fade12ccf01ba65e2043e4efba71161c9c7
Joshua Wong <Josh@joshua-wongs-macbook-pro.local>
2016-01-06 19:24:46 -0800

    set up testing suite

---
+++ karma.conf.js
@@ -0,0 +1,42 @@
+// Karma configuration
+
+module.exports = function (config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: './',
+
+    // testing frameworks to use
+    frameworks: ['mocha', 'chai', 'sinon'],
+
+    // list of files / patterns to load in the browser. order matters!
+    files: [
+      // angular source
+      'public/lib/angular/angular.js',
+      'public/lib/angular-route/angular-route.js',
+      'public/lib/angular-mocks/angular-mocks.js',
+
+      // our app code
+      'public/js/*',
+
+      // our spec files - in order of the README
+      // 'specs/client/authControllerSpec.js',
+      // 'specs/client/servicesSpec.js',
+      'specs/client/routingSpec.js'
+
+    ],
+
+    // test results reporter to use
+    reporters: ['nyan','unicorn'],
+
+    // start these browsers. PhantomJS will load up in the background
+    browsers: ['PhantomJS'],
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+    // if true, Karma exits after running the tests.
+    singleRun: true
+
+  });
+};
