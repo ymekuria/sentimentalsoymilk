72f697139066623f2a7effa80e33a8d15c96092e
Justin Mancherje <mancherje.justin@gmail.com>
2016-01-06 17:49:39 -0800

    begin basic structure of angular front end. add app module add controller for login page add template for login page

---
+++ public/js/controllers/loginController.js
@@ -0,0 +1,17 @@
+angular.module('app.login', [])
+
+.controller('LoginController', function ($scope, $http) {
+
+
+  // login function to be called when input form submitted
+  $scope.login = function () {
+    var userData = {
+      "username":$scope.username,
+      "password":$scope.password
+    }
+    $http.post('/login', userData)
+      .success(function () {
+        // redirect to main page
+      })
+  };
+})
