daa4f806b82c9e40c1cbdd0168b019d4b8fa0ee2
Liza Lopez <liza.b.lopez@gmail.com>
2016-01-07 18:08:20 -0800

    Serving filtered data to client on get activities request

--- server/server.js
+++ server/server.js
@@ -5,12 +5,12 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-require('./routers/router.js')(app, express);
 var port = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+require('./routers/router.js')(app, express);
 require('./models/dbroutes.js')(app, express);
 
 app.use(express.static(__dirname+'/../public'));
