d7da75f150e8832d2aee3da22475fda4f2cbf481
Justin Mancherje <mancherje.justin@gmail.com>
2016-01-11 10:27:29 -0800

    route to indevidual trip page, set up skeleton for receiving data. update splash page

--- public/js/controllers/myTripController.js
+++ public/js/controllers/myTripController.js
@@ -1,8 +1,16 @@
 angular.module('app.mytrips', ['app.services'])
 
-.controller('MyTripsController', function ($scope, $http, ActivitiesData) {
+.controller('MyTripsController', function ($scope, $http, ActivitiesData, $location) {
   ActivitiesData.getTrips()
   .then(function(results){
     $scope.tripResults = results.data;
-  })
+  });
+
+  $scope.viewTrip = function (index) {
+    $location.path('/trip/' + id);
+    $scope.id = $scope.tripResults[index]._id;
+    console.log($scope.id);
+    // get request to get info for trip
+    ActivitiesData.getTripActivities(index);
+  };
 })
---
+++ public/js/controllers/tripViewController.js
@@ -0,0 +1,11 @@
+angular.module('app.trip', ['app.services'])
+
+.controller('TripController', function ($scope, $http, ActivitiesData, $routeParams) {
+
+  $scope.id = $routeParams.id;
+
+  ActivitiesData.getTripActivities($scope.id, function (data) {
+    $scope.activities = data;
+  });
+
+})
