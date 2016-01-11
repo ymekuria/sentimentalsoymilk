73eb96ccba49a646acccfe77d7e656f91df79643
Justin Mancherje <mancherje.justin@gmail.com>
2016-01-10 22:44:29 -0800

    update trips model to accept image in schema. Update controller to attach image

--- server/models/trips.js
+++ server/models/trips.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 var tripsSchema = mongoose.Schema({
   name: String,
   destination: Array,
+  image: String,
   //keyword: drinking, bars
   //users
   activities : [{ type: mongoose.Schema.ObjectId,
